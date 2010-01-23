cd23ec12b4e247fa94e991eea766b1a24d174327
Tuomas Jomppanen <tuomasj@gmail.com>
2010-01-23 14:05:47 +0200

    Using image instead of resource because path problems

--- spaceinvaders/spaceinvaders.py
+++ spaceinvaders/spaceinvaders.py
@@ -7,9 +7,12 @@ window = pyglet.window.Window()
 
 keys = pyglet.window.key.KeyStateHandler()
 window.push_handlers(keys)
+print("script home:" + pyglet.resource.get_script_home() )
 
-player_image = pyglet.resource.image('player.png')
+# player_image = pyglet.resource.image('player.png')
+player_image = pyglet.image.load('player.png')
 player = player.Player( player_image, 320, 200)
+
 
 @window.event
 def on_draw():
